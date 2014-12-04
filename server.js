a39917f8683208dce9c40a555ba9494666b13a56
Valentyn Boginskey <poison5151@gmail.com>
2014-12-04 10:47:31 -0800

    Completed basic server skeleton

---
+++ server.js
@@ -0,0 +1,21 @@
+var express        = require('express');
+var bodyParser     = require('body-parser');
+var morgan         = require('morgan');
+
+var app            = express();
+
+// var db = require('./config/db');
+// mongoose.connect(db.url);
+
+var port = process.env.PORT || 8080;
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
+
+require('./app/routes')(app);
+
+app.listen(port);
+
+exports = module.exports = app;
