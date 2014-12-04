ff4762d8aa661d1b1a1d2360cc46989127750d59
Robert Niimi <Robert Niimi>
2014-12-04 15:46:41 -0800

    added veto functions

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -1,5 +1,9 @@
 angular.module('cleaver.controllers', [])
 
 .controller('MainController', function($scope, Rec) {
-  $.extend($scope, Rec);
+  angular.extend($scope, Rec);
+  var currentId = $routeParams.id;
+  getRestaurants(currentId);
+  // veto(business_id);
+  // postLocation(location);
 });
