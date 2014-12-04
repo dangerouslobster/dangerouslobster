3f54a4dc0612e1580f260b2c9631833556363ea8
Robert Niimi <Robert Niimi>
2014-12-04 14:14:42 -0800

    refactored Rec factory

--- public/js/services/rec.js
+++ public/js/services/rec.js
@@ -3,20 +3,33 @@ angular.module('cleaverApp', [])
 // this factory handles requests between the client and server
 
 .factory('Rec', ['$http', function($http) {
+  var restaurants;
+
+  var getRestaurants = function (uniqueID) {
+    return $http({
+      method: 'GET',
+      url: '/api/' + uniqueID
+    })
+    .then(function (resp) {
+      console.log("get restaurants", resp.data);
+      restaurants = resp.data;
+    });
+  };
+
+  var addLocation = function() {
+    return $http({
+      method: 'POST',
+      url: '/api/location',
+      // data: { url:link }
+    }).then(function(resp) {
+      console.log("added location", resp);
+    });
+  };
+
   return {
-    getRestaurants: function(uniqueID) {
-      var promise = $http.get('/api/' + uniqueID)
-      .success(function(data, status, config, headers){
-        restaurantData = data;
-      })
-      .error(function(){
-        console.error('error getting restaurants');
-      });
-    },
-    
-    postLocation: function(data) {
-      $http.post('/location', data)
-    },
-    restaurants: function() { return restaurantData; };
-  }
+    getRestaurants: getRestaurants,
+    addLocation: addLocation,
+    restaurants: restaurants
+  };
+
 }]);
