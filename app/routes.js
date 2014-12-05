4e40ac872a13898feabc3aae0b77994baed3a64c
Valentyn Boginskey <poison5151@gmail.com>
2014-12-04 16:15:44 -0800

    Fixed variable name for RecSession instance

--- app/routes.js
+++ app/routes.js
@@ -3,8 +3,8 @@ var models = require('./models.js');
 
 module.exports = function(app) {
   app.post('/location', function(req, res) {
-    var location = new models.RecSession(req.body.location, utils.generateUID());
-    location.getYelpData(function(err, data, yelpRes) {
+    var recSession = new models.RecSession(req.body.location, utils.generateUID());
+    recSession.getYelpData(function(err, data, yelpRes) {
       res.json({locationData: location, yelpData: location.yelpData});
     })
   });
