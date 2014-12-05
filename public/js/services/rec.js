ae0a3c09b47fc21ae67a6bdd5b95dcdd66cf85e8
Robert Niimi <Robert Niimi>
2014-12-04 17:12:43 -0800

    fixed merge issue

--- public/js/services/rec.js
+++ public/js/services/rec.js
@@ -47,7 +47,6 @@ angular.module('cleaver.services', [])
   //   });
   // };
 
-<<<<<<< HEAD
   return {
     getRestaurants: getRestaurants,
     postLocation: postLocation,
@@ -57,38 +56,3 @@ angular.module('cleaver.services', [])
   };
 });
 // }]);
-=======
-// this factory handles requests between the client and server
-
-.factory('Rec', ['$http', function($http) {
-  var restaurants;
-
-  var getRestaurants = function (uniqueID) {
-    return $http({
-      method: 'GET',
-      url: '/api/' + uniqueID
-    })
-    .then(function (resp) {
-      console.log("get restaurants", resp.data);
-      restaurants = resp.data;
-    });
-  };
-
-  var addLocation = function() {
-    return $http({
-      method: 'POST',
-      url: '/api/location',
-      // data: { url:link }
-    }).then(function(resp) {
-      console.log("added location", resp);
-    });
-  };
-
-  return {
-    getRestaurants: getRestaurants,
-    addLocation: addLocation,
-    restaurants: restaurants
-  };
-
-}]);
->>>>>>> origin/master
