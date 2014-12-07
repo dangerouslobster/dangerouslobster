dbda294655f2a57ded2494ef4735be19ed12a51e
Robert Niimi <Robert Niimi>
2014-12-06 17:02:01 -0800

    fixed nested uiview

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -3,12 +3,11 @@ angular.module('cleaver.controllers', [])
 .controller('MainController', function($scope, $state, Rec) {
   angular.extend($scope, Rec);
 
-
   //TODO: clear input box on enter
   $scope.enter = function(keyEvent, location) {
     if (location && keyEvent.which === 13) {
       $scope.postLocation(location);
-      $state.go('rec');
+      $state.go('home.recs');
     }
   };
 
