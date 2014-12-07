45a9f937b303b838eb591fac016ed6413d71f0bf
John Tan <tantai.john@gmail.com>
2014-12-07 12:26:19 -0800

    Added function for formatting undo button text

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -2,6 +2,21 @@ angular.module('cleaver.controllers', [])
 
 .controller('MainController', function($scope, $state, Rec) {
   angular.extend($scope, Rec);
+  $scope.formatUndo = function(){
+    if($scope.lastVeto){
+      if($scope.lastVeto.key === 'category'){
+        return $scope.lastVeto.val;
+      }else{
+        var results = [];
+        var splitUp = $scope.lastVeto.val.split('-');
+        splitUp.forEach(function(el){
+          results.push(el.charAt(0).toUpperCase() + el.slice(1));
+        })
+        return results.join(' ');
+      }
+    }
+  };
+
   $scope.vetoRestaurant = function(restaurantID){
     Rec.vetoRestaurant(restaurantID);
     $scope.lastVeto = {
