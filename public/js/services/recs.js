56733a7645ff73f4690db621d2823da72b97db2d
Robert Niimi <Robert Niimi>
2014-12-08 00:07:13 -0800

    stopped spinner from turning into search icon prematurely

--- public/js/services/recs.js
+++ public/js/services/recs.js
@@ -32,7 +32,6 @@ angular.module('cleaver.services', ['firebase'])
     }).success(function(resp) {
       data.id = uniqueID;
       data.restaurants = resp.businesses;
-      angular.element(document.querySelector('i')).toggleClass('search').toggleClass('spinner');
 
       for (var key in data.restaurants) {
         data.restaurants[key].distance = calculateDistance(resp.region.center.longitude,resp.region.center.latitude,
@@ -53,7 +52,7 @@ angular.module('cleaver.services', ['firebase'])
     }).then(function(resp) {
       data.id = resp.data.uniqueID;
       data.restaurants = resp.data.yelpData.businesses;
-      angular.element(document.querySelector('i')).toggleClass('search').toggleClass('spinner');
+      setTimeout(function() {angular.element(document.querySelector('i')).toggleClass('search').toggleClass('spinner')}, 500);
 
       for (var key in data.restaurants) {
         data.restaurants[key].distance = calculateDistance(resp.data.yelpData.region.center.longitude,resp.data.yelpData.region.center.latitude,
