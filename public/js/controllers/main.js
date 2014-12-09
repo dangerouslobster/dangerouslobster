a26ac65b85b3f178c3b7608e1891ff0e326a9b4f
Robert Niimi <Robert Niimi>
2014-12-08 16:56:55 -0800

    added documentation

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -2,23 +2,28 @@ angular.module('cleaver.controllers', ['ngAutocomplete'])
 
 .controller('MainController', function($scope, $state, Rec) {
   angular.extend($scope, Rec);
+
   $scope.stateIs = $state.is;
+  $scope.lastVeto = {};
   $scope.inputOptions = {
     types: 'geocode',
     country: 'us',
     watchEnter: true
   };
-  $scope.lastVeto = {};
 
   angular.element(document.body).css('background-image', 'url("../img/background_' +
     (Math.floor(Math.random() * 5) + 1) + '.jpg")');
 
-  $scope.undoIt = function(){
-    Rec.undo($scope.lastVeto);
-    $scope.lastVeto = {};
-    angular.element(document.querySelectorAll('#undoButton')).addClass('disabled');
+  // takes "enter" key event and submits the user's location input
+  $scope.enterLocation = function(keyEvent) {
+    if ($scope.details && (keyEvent === undefined || keyEvent.which === 13)) {
+      $scope.postLocation($scope.details.formatted_address.slice(0, -5));
+      $scope.details = null;
+      angular.element(document.querySelector('i')).toggleClass('search').toggleClass('spinner loading');
+    }
   };
 
+  // adds vetoed restaurant to the veto array
   $scope.vetoRestaurant = function(restaurantID){
     Rec.vetoRestaurant(restaurantID);
     $scope.lastVeto = {
@@ -27,6 +32,7 @@ angular.module('cleaver.controllers', ['ngAutocomplete'])
     };
   };
 
+  // adds the vetoed category to veto array
   $scope.vetoCategory = function(category){
     Rec.vetoCategory(category);
     $scope.lastVeto = {
@@ -35,13 +41,11 @@ angular.module('cleaver.controllers', ['ngAutocomplete'])
     };
   };
 
-  // submits the user's location input
-  $scope.enterLocation = function(keyEvent) {
-    if ($scope.details && (keyEvent === undefined || keyEvent.which === 13)) {
-      $scope.postLocation($scope.details.formatted_address.slice(0, -5));
-      $scope.details = null;
-      angular.element(document.querySelector('i')).toggleClass('search').toggleClass('spinner loading');
-    }
+  // removes the last vetoed category or restaurant from the veto list
+  $scope.undoIt = function(){
+    Rec.undo($scope.lastVeto);
+    $scope.lastVeto = {};
+    angular.element(document.querySelectorAll('#undoButton')).addClass('disabled');
   };
 
 });
